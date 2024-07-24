4dfec424625bb5650df8f4b2d913279168ffe4a0
Yacouba Simboro <yacouba.simboro@nebrata.com>
2024-07-24 12:07:22 +0100

    config

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async(req, res) => {
-    res.sendFile(path.join(__dirname, '\dist\projet-vagrant-frontend-m1-ibam\browser', 'index.html'));
+    //res.sendFile(path.join(__dirname, 'dist/projet-vagrant-frontend-m1-ibam/browser', 'index.html'));
+    res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 // set port, listen for requests
 //const PORT = process.env.PORT || 8080;
-const PORT = 4200;
+const PORT = 4300;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
