bd9c980cd6def0246bc10a542f91d3c187c78508
Yacouba Simboro <yacouba.simboro@nebrata.com>
2024-07-25 16:45:05 +0100

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'http://192.168.0.202:8080';
 export const environment = {
   apiUrl: baseUrl,
   production: true,
