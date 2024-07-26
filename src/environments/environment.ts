b20356187d313f040f8d08c44c5329b0831e193d
simboroyacouba <80728664+simboroyacouba@users.noreply.github.com>
2024-07-26 23:58:52 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'http://192.168.0.202:8080';
 export const environment = {
   apiUrl: baseUrl,
   production: true,
   PUBLIC: {
-    TASK_API: "/tasks",
+    TASK_API: baseUrl+"/tasks",
   },
  
   FRONTEND_ROUTES: {
